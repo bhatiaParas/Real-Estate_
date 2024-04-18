5356d42ff1a762c5e970ab984e6a56ea85d26bba
bhatiaParas <147644425+bhatiaParas@users.noreply.github.com>
2024-04-18 04:34:32 +0000

    yes i did

--- Real-Estate-Project/src/routes/profilePage/profilePage.jsx
+++ Real-Estate-Project/src/routes/profilePage/profilePage.jsx
@@ -1,3 +1,4 @@
+
 import Chat from "../../components/chat/Chat";
 import List from "../../components/list/List";
 import "./profilePage.scss";
@@ -39,7 +40,7 @@ function ProfilePage() {
       </div>
       <div className="chatContainer">
         <div className="wrapper">
-          <Chat/>
+         <Chat />
         </div>
       </div>
     </div>
