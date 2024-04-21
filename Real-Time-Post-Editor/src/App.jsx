e7616d6a6a36a53ee80d9cf6adfbf0e325e62c4c
bhatiaParas <147644425+bhatiaParas@users.noreply.github.com>
2024-04-21 13:46:16 +0000

    2

--- Real-Time-Post-Editor/src/App.jsx
+++ Real-Time-Post-Editor/src/App.jsx
@@ -1,3 +1,4 @@
+import './app.scss';
 import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import './App.css'
@@ -5,7 +6,6 @@ import authService from "./appwrite/auth"
 import {login, logout} from "./store/authSlice"
 import { Footer, Header } from './components'
 import { Outlet } from 'react-router-dom'
-
 function App() {
   const [loading, setLoading] = useState(true)
   const dispatch = useDispatch()
@@ -23,13 +23,22 @@ function App() {
   }, [])
   
   return !loading ? (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='min-h-screen flex flex-wrap content-between bg-white bg-opacity-50'>
       <div className='w-full block'>
+        <div className="header">
         <Header />
+
+        </div>
         <main>
-        TODO:  <Outlet />
+          <div className="beech">
+
+          <Outlet />
+          </div>
         </main>
+        <div className="footer">
         <Footer />
+
+        </div>
       </div>
     </div>
   ) : null
